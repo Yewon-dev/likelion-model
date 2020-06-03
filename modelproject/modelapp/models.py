855000068e6a3123ae16671470412d6cfc8ddb69
Yewon <kyw20160785@gmail.com>
2020-06-03 19:34:10 +0900

    model test

---
+++ modelproject/modelapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    date = models.DateTimeField('date')
+
+    # admin 글 제못 표시하기 위한 함수
+    def __str__(self):
+        return self.title
+Class Article(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    date = models.DateTimeField('date')
+
+    def __str__(self):
+        return self.title    
